bb07c06c78eefe14615fc6ca0f9236df739aecc5
EugeneZakhno <zohan1234567@gmail.com>
2019-11-10 15:14:33 +0200

    second commit into TDD

--- WyCash/src/main/java/io/github/eugenezakhno/Runner.java
+++ WyCash/src/main/java/io/github/eugenezakhno/Runner.java
@@ -1,16 +1,17 @@
 package io.github.eugenezakhno;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println(testMultiplication());
+        testMultiplication();
     }
 
     public static void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
-        Assert.assertEquals(10, five.amount);
+        assertEquals(10, five.amount);
+
     }
 
 }
